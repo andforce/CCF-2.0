2622a305276b9c68ace4d4a40039aa3eb4b65453
andforce <86118@163.com>
2018-04-29 10:26:41 +0800

    refactor

--- Custom/crsky/CrskyForumConfig.h
+++ Custom/crsky/CrskyForumConfig.h
@@ -9,6 +9,58 @@
 #import <Foundation/Foundation.h>
 #import "ForumConfigDelegate.h"
 
+#define BBS_HOST @"http://bbs.crsky.com/"
+
+#define ARCHIVE BBS_HOST@"index.php?skinco=wind"
+
+#define SEARCH BBS_HOST@"search.php?"
+
+#define SEARCH_WITH_SEARCHID SEARCH@"step=2&sid=%@&seekfid=all&page=%d"
+
+#define READ BBS_HOST@"read.php?tid=%@"
+
+#define FAV_THREAD BBS_HOST@"pw_ajax.php?action=favor&type=0&nowtime=%ldl&tid=%@"
+
+#define UNFAV BBS_HOST@"u.php?action=favor&"
+
+#define LST_FAV BBS_HOST@"u.php?action=favor&uid=%d"
+
+#define FORUM_DIS BBS_HOST@"thread.php?fid=%@&page=%d"
+
+#define SEARCH_NEW BBS_HOST@"search.php?sch_time=all&orderway=lastpost&asc=desc&newatc=1"
+
+#define REPLY BBS_HOST@"post.php?"
+
+#define QUOTE_REPLY BBS_HOST@"post.php?action=quote&fid=%d&tid=%d&pid=%d"
+
+#define SHOW_THREAD READ@"&fpage=0&toread=&page=%d"
+
+#define COPY_URL READ@"#%@"
+
+#define MEMBER BBS_HOST@"u.php?action=show&uid=%@"
+
+#define MESSAGE BBS_HOST@"message.php"
+
+#define WRITE_MESSAGE MESSAGE@"?action=write"
+
+#define RECEIVE_BOX MESSAGE@"?action=receivebox&page=%d"
+
+#define SEND_BOX MESSAGE@"?action=sendbox&page=%d"
+
+#define DEL_RECEIVE_BOX MESSAGE@"?action=receivebox"
+
+#define DEL_SEND_BOX MESSAGE@"?action=sendbox"
+
+#define READ_PUBLIC_MESSAGE MESSAGE@"?action=readpub&mid=%d"
+
+#define READ_PRI_MESSAGE MESSAGE@"?action=read&mid=%d"
+
+#define READ_SEND_PRI_MSG MESSAGE@"?action=readsnd&mid=%d"
+
+#define REPLY_MSG_PRE MESSAGE@"?action=write&remid=%d"
+
+#define LIST_USER_THREAD BBS_HOST@"u.php?action=topic&uid=%@&page=%d"
+
 @interface CrskyForumConfig : NSObject<ForumConfigDelegate>
 
 @end
--- Custom/crsky/CrskyForumConfig.m
+++ Custom/crsky/CrskyForumConfig.m
@@ -9,18 +9,18 @@
 #import "CrskyForumConfig.h"
 #import "DeviceName.h"
 
-@implementation CrskyForumConfig{
+@implementation CrskyForumConfig {
     NSURL *_forumURL;
 }
 
 - (instancetype)init {
     self = [super init];
-    _forumURL = [NSURL URLWithString:@"http://bbs.crsky.com/"];
+    _forumURL = [NSURL URLWithString:BBS_HOST];
     return self;
 }
 
 - (UIColor *)themeColor {
-    return [[UIColor alloc] initWithRed:56.f/255.f green:133.f/255.f blue:233.f/255.f alpha:1];;
+    return [[UIColor alloc] initWithRed:56.f / 255.f green:133.f / 255.f blue:233.f / 255.f alpha:1];;
 }
 
 - (NSURL *)forumURL {
@@ -28,7 +28,7 @@
 }
 
 - (NSString *)archive {
-    return @"http://bbs.crsky.com/index.php?skinco=wind";
+    return ARCHIVE;
 }
 
 - (NSString *)cookieUserIdKey {
@@ -39,24 +39,174 @@
     return @"217cd_ol_offset";
 }
 
-- (NSString *)newattachmentForThread:(int)threadId time:(NSString *)time postHash:(NSString *)postHash {
-    return nil;
-}
-
-- (NSString *)newattachmentForForum:(int)forumId time:(NSString *)time postHash:(NSString *)postHash {
-    return nil;
-}
-
-- (NSString *)newattachment {
-    return nil;
-}
 
 - (NSString *)search {
-    return @"http://bbs.crsky.com/search.php?";
+    return SEARCH;
 }
 
 - (NSString *)searchWithSearchId:(NSString *)searchId withPage:(int)page {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/search.php?step=2&sid=%@&seekfid=all&page=%d", searchId, page];
+    return [NSString stringWithFormat:SEARCH_WITH_SEARCHID, searchId, page];
+}
+
+
+- (NSString *)favThreadWithIdPre:(NSString *)threadId {
+    return [NSString stringWithFormat:READ, threadId];
+}
+
+- (NSString *)favThreadWithId:(NSString *)threadId {
+    NSDate *date = [NSDate date];
+    NSInteger timeStamp = (NSInteger) [date timeIntervalSince1970];
+    return [NSString stringWithFormat:FAV_THREAD, (long) timeStamp, threadId];
+}
+
+- (NSString *)unFavorThreadWithId:(NSString *)threadId {
+    return UNFAV;
+}
+
+- (NSString *)listFavorThreads:(int)userId withPage:(int)page {
+    return [NSString stringWithFormat:LST_FAV, userId];
+}
+
+- (NSString *)forumDisplayWithId:(NSString *)forumId withPage:(int)page {
+    return [NSString stringWithFormat:FORUM_DIS, forumId, page];
+}
+
+- (NSString *)searchNewThread:(int)page {
+    return SEARCH_NEW;
+}
+
+- (NSString *)replyWithThreadId:(int)threadId forForumId:(int)forumId replyPostId:(int)postId {
+    return REPLY;
+}
+
+- (NSString *)quoteReply:(int)fid threadId:(int)threadId postId:(int)postId {
+    return [NSString stringWithFormat:QUOTE_REPLY, fid, threadId, postId];
+}
+
+- (NSString *)showThreadWithThreadId:(NSString *)threadId withPage:(int)page {
+    return [NSString stringWithFormat:SHOW_THREAD, threadId, page];
+}
+
+- (NSString *)showThreadWithP:(NSString *)p {
+    return nil;
+}
+
+- (NSString *)copyThreadUrl:(NSString *)threadId withPostId:(NSString *)postId withPostCout:(int)postCount {
+    NSString *fixPostId = postId;
+    if ([fixPostId isEqualToString:@"0"]) {
+        fixPostId = @"tpc";
+    }
+    return [NSString stringWithFormat:COPY_URL, threadId, fixPostId];
+}
+
+
+- (NSString *)avatar:(NSString *)avatar {
+    return avatar;
+}
+
+- (NSString *)avatarBase {
+    return @"";
+}
+
+- (NSString *)avatarNo {
+    return @"/no_avatar.gif";
+}
+
+- (NSString *)memberWithUserId:(NSString *)userId {
+    return [NSString stringWithFormat:MEMBER, userId];
+}
+
+- (NSString *)createNewThreadWithForumId:(NSString *)forumId {
+    return REPLY;
+}
+
+
+- (NSString *)privateWithType:(int)type withPage:(int)page {
+    //  0 receive box
+    if (type == 0) {
+        return [NSString stringWithFormat:RECEIVE_BOX, page];
+    } else {
+        //  -1 send box
+        return [NSString stringWithFormat:SEND_BOX, page];
+    }
+}
+
+- (NSString *)deletePrivateWithType:(int)type {
+    if (type == 0) {
+        return DEL_RECEIVE_BOX;
+    } else {
+        return DEL_SEND_BOX;
+    }
+}
+
+- (NSString *)privateShowWithMessageId:(int)messageId withType:(int)type {
+    // 0 群发公共消息 1 普通收件 2 发给别人的消息
+    if (type == 0) {
+        return [NSString stringWithFormat:READ_PUBLIC_MESSAGE, messageId];
+    } else if (type == 1) {
+        return [NSString stringWithFormat:READ_PRI_MESSAGE, messageId];
+    } else if (type == 2) {
+        return [NSString stringWithFormat:READ_SEND_PRI_MSG, messageId];
+    } else {
+        return nil;
+    }
+}
+
+- (NSString *)privateReplyWithMessageIdPre:(int)messageId {
+    return [NSString stringWithFormat:REPLY_MSG_PRE, messageId];
+}
+
+- (NSString *)privateReplyWithMessage {
+    return MESSAGE;
+}
+
+- (NSString *)privateNewPre {
+    return WRITE_MESSAGE;
+}
+
+- (NSString *)loginControllerId {
+    return @"CrskyLoginViewController";
+}
+
+- (NSString *)listUserThreads:(NSString *)userId withPage:(int)page {
+    return [NSString stringWithFormat:LIST_USER_THREAD, userId, page];
+}
+
+- (NSString *)signature {
+    NSString *phoneName = [DeviceName deviceNameDetail];
+    NSString *signature = [NSString stringWithFormat:@"\n\n发自 %@ 使用 霏凡客户端", phoneName];
+    return signature;
+}
+
+//----------------
+
+- (NSString *)enterCreateNewThreadWithForumId:(NSString *)forumId {
+    return nil;
+}
+
+
+- (NSString *)login {
+    return nil;
+}
+
+- (NSString *)loginvCode {
+    return nil;
+}
+
+- (NSString *)forumDisplayWithId:(NSString *)forumId {
+    return nil;
+}
+
+- (NSString *)favoriteForums {
+    return nil;
+}
+
+- (NSString *)report {
+    return nil;
+}
+
+- (NSString *)reportWithPostId:(int)postId {
+    return nil;
 }
 
 - (NSString *)searchThreadWithUserId:(NSString *)userId {
@@ -79,161 +229,16 @@
     return nil;
 }
 
-- (NSString *)favThreadWithIdPre:(NSString *)threadId {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/read.php?tid=%@", threadId];
-}
-
-- (NSString *)favThreadWithId:(NSString *)threadId {
-    NSDate *date = [NSDate date];
-    NSInteger timeStamp = (NSInteger) [date timeIntervalSince1970];
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/pw_ajax.php?action=favor&type=0&nowtime=%ldl&tid=%@", (long)timeStamp, threadId];
-}
-
-- (NSString *)unFavorThreadWithId:(NSString *)threadId {
-    return @"http://bbs.crsky.com/u.php?action=favor&";
-}
-
-- (NSString *)listFavorThreads:(int)userId withPage:(int)page {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/u.php?action=favor&uid=%d", userId];
-}
-
-- (NSString *)forumDisplayWithId:(NSString *)forumId {
+- (NSString *)newattachmentForThread:(int)threadId time:(NSString *)time postHash:(NSString *)postHash {
     return nil;
 }
 
-- (NSString *)forumDisplayWithId:(NSString *)forumId withPage:(int)page {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/thread.php?fid=%@&page=%d", forumId, page];
-}
-
-- (NSString *)searchNewThread:(int)page {
-    return @"http://bbs.crsky.com/search.php?sch_time=all&orderway=lastpost&asc=desc&newatc=1";
-}
-
-- (NSString *)replyWithThreadId:(int)threadId forForumId:(int)forumId replyPostId:(int)postId {
-    return @"http://bbs.crsky.com/post.php?";
-}
-
-- (NSString *)quoteReply:(int)fid threadId:(int)threadId postId:(int)postId {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/post.php?action=quote&fid=%d&tid=%d&pid=%d", fid, threadId, postId];
-}
-
-- (NSString *)showThreadWithThreadId:(NSString *)threadId withPage:(int)page {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/read.php?tid=%@&fpage=0&toread=&page=%d",threadId, page];
-}
-
-- (NSString *)showThreadWithP:(NSString *)p {
+- (NSString *)newattachmentForForum:(int)forumId time:(NSString *)time postHash:(NSString *)postHash {
     return nil;
 }
 
-- (NSString *)copyThreadUrl:(NSString *)threadId withPostId:(NSString *)postId withPostCout:(int)postCount {
-    NSString *fixpostId = postId;
-    if ([fixpostId isEqualToString:@"0"]){
-        fixpostId = @"tpc";
-    }
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/read.php?tid=%@#%@",threadId, fixpostId];
-}
-
-
-- (NSString *)avatar:(NSString *)avatar {
-    return avatar;
-}
-
-- (NSString *)avatarBase {
-    return @"";
-}
-
-- (NSString *)avatarNo {
-    return @"/no_avatar.gif";
-}
-
-- (NSString *)memberWithUserId:(NSString *)userId {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/u.php?action=show&uid=%@",userId];
-}
-
-- (NSString *)login {
+- (NSString *)newattachment {
     return nil;
-}
-
-- (NSString *)loginvCode {
-    return nil;
-}
-
-- (NSString *)createNewThreadWithForumId:(NSString *)forumId {
-    return @"http://bbs.crsky.com/post.php?";
-}
-
-- (NSString *)enterCreateNewThreadWithForumId:(NSString *)forumId {
-    return nil;
-}
-
-
-- (NSString *)privateWithType:(int)type withPage:(int)page {
-    //  0 receive box
-    if (type == 0){
-        return [NSString stringWithFormat:@"http://bbs.crsky.com/message.php?action=receivebox&page=%d", page];
-    } else{
-    //  -1 send box
-        return [NSString stringWithFormat:@"http://bbs.crsky.com/message.php?action=sendbox&page=%d", page];
-    }
-}
-
-- (NSString *)deletePrivateWithType:(int)type {
-    if (type == 0){
-        return @"http://bbs.crsky.com/message.php?action=receivebox";
-    } else {
-        return @"http://bbs.crsky.com/message.php?action=sendbox";
-    }
-}
-
-- (NSString *)privateShowWithMessageId:(int)messageId withType:(int)type {
-    // 0 群发公共消息 1 普通收件 2 发给别人的消息
-    if (type == 0){
-        return [NSString stringWithFormat:@"http://bbs.crsky.com/message.php?action=readpub&mid=%d",messageId];
-    } else  if(type == 1){
-        return [NSString stringWithFormat:@"http://bbs.crsky.com/message.php?action=read&mid=%d",messageId];
-    } else if(type == 2){
-        return [NSString stringWithFormat:@"http://bbs.crsky.com/message.php?action=readsnd&mid=%d",messageId];
-    } else {
-        return nil;
-    }
-}
-
-- (NSString *)privateReplyWithMessageIdPre:(int)messageId {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/message.php?action=write&remid=%d", messageId];
-}
-
-- (NSString *)privateReplyWithMessage {
-    return @"http://bbs.crsky.com/message.php";
-}
-
-- (NSString *)privateNewPre {
-    return @"http://bbs.crsky.com/message.php?action=write";
-}
-
-- (NSString *)favoriteForums {
-    return nil;
-}
-
-- (NSString *)report {
-    return nil;
-}
-
-- (NSString *)reportWithPostId:(int)postId {
-    return nil;
-}
-
-- (NSString *)loginControllerId {
-    return @"CrskyLoginViewController";
-}
-
-- (NSString *)listUserThreads:(NSString *)userId withPage:(int)page {
-    return [NSString stringWithFormat:@"http://bbs.crsky.com/u.php?action=topic&uid=%@&page=%d" ,userId, page];
-}
-
-- (NSString *)signature {
-    NSString *phoneName = [DeviceName deviceNameDetail];
-    NSString *signature = [NSString stringWithFormat:@"\n\n发自 %@ 使用 霏凡客户端", phoneName];
-    return signature;
 }
 
 @end
